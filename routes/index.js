91113a55d39df37d3d7a2f9c57f33d7adef15638
Jack Lucky <jack.lucky.iv@gmail.com>
2019-06-29 23:18:08 -0700

    First commit

---
+++ routes/index.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+
+const moment = require('moment');
+const schedule = require('node-schedule');
+const twilio = require('twilio');
+var express = require('express');
+
+var router = express.Router();
+
+const jack = process.env.JACK;
+const fromNumber = process.env.CJA_BOARD_BROADCAST_NUMBER;
+const client = twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
+const notify = client.notify.services(process.env.CJA_BOARD_BROADCAST_SERVICE_SID);
+
+const broadcastSuccessMessage = 'Boom! Message broadcast to all subscribers.\n'
+const broadcastFailMessage = 'Well this is awkward. Your message failed to send, try again later.\n'
+
+function broadcastMessage() {
+  notify.notifications.create({
+    tag: 'all',
+    body: body
+  }).then((response) => {
+    console.log(broadcastSuccessMessage, response);
+  }).catch(err => {
+    console.log(broadcastFailMessage, err);
+  })
+}
+
+function sendMessage(body) {
+  client.messages
+    .create({
+      body: body,
+      from: fromNumber,
+      to: jack
+    })
+    .then(message => console.log(message.sid));
+}
+function scheduleMessage(dateString, body) {
+  const date = dateString ? moment(dateString) : moment();
+  const sendDate = date.add(3, 'minute');
+  const content = body ? body : `This message should be received at ${sendDate.format('H:mm')}.`;
+  schedule.scheduleJob(sendDate.toDate(), sendMessage(content));
+}
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/send', function (req, res, next) {
+  scheduleMessage();
+});
+
+module.exports = router;
